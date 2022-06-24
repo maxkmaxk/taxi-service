0b572c5e94535a597c6f7d6fd8555d72e053c513
Maksym Pokras <mxpokras@gmail.com>
2022-06-24 15:33:21 +0200

    logger removed from imports

--- src/main/java/taxi/dao/CarDaoImpl.java
+++ src/main/java/taxi/dao/CarDaoImpl.java
@@ -9,8 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import taxi.exception.DataProcessingException;
 import taxi.lib.Dao;
 import taxi.model.Car;
